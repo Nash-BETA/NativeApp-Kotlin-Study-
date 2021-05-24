aa70d4d5f70581081b84b96f761d2326105043e3
Nash-BETA <nash.0cal@gmail.com>
2021-05-24 20:03:38 +0900

    first

--- session3/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ session3/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.hellosample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ボタンの取得
+        val btClick = findViewById<Button>(R.id.btClick)
+        //名前表示のクラスをインスタンス化して取得
+        val listener = HelloListener()
+        //setOnClickListenerの中にlistenerのインスタンスを入れると動く
+        btClick.setOnClickListener(listener)
     }
 
-    //インナークラス
+    //ボタンを押したときに名前を表示させる処理（インナークラス）
     private inner class HelloListener: View.OnClickListener{
         override fun onClick(view:View){
-            var inpur = findViewById<EditTect>(R.id.etName);
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+            val inputStr = input.text.toString()
+            output.text = inputStr + "さん。こんにちは！"
         }
     }
 }
