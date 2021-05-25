74b65cc207c2a838a77b39aa998e69fb753b2e48
sh.nakayama <sh.nakayama@raxus-create.co.jp>
2021-05-25 19:35:21 +0900

    5.1

--- session5/app/src/main/java/com/example/session5/MainActivity.kt
+++ session5/app/src/main/java/com/example/session5/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.session5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ListViewオブジェクト
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.onItemClickListener = ListItemClickListener()
+        //
+    }
+
+    //タップされたときの挙動
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener{
+        //AdapterViewクラスのOnItemClickListeneインターフェイスの継承書き換え
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            //parentはタップされたもの
+            //テキストを取得
+            val item = parent?.getItemAtPosition(position) as String
+            //テキストを定義
+            val show_food = "あなたが選んだ定食：$item"
+            //Toast.makeText(引数１，引数２，引数３)
+            Toast.makeText(this@MainActivity,show_food,Toast.LENGTH_LONG).show()
+        }
     }
 }
